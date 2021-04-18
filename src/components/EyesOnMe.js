011cf8d77e6a8822ea2f84c40ad042a82c69cc97
theo <teddyhoover@gmail.com>
2021-04-18 11:16:11 -0500

    commit tree f16371e7d135b54392c1c710c327cd20fb6b5562

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import { Component } from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+    render(){
+        return(
+            <button
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+            />
+        )
+    }
+
+    handleFocus = () => {
+        console.log('Good!')
+    }
+    handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+}
